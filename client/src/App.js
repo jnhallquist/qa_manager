b85eedfe56224c6e894d0bdb9c0670da1c55594c
jnhallquist <jnhallquist@gmail.com>
2020-07-05 11:54:37 -0700

    Update theme

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { purple } from '@material-ui/core/colors'
 
 import TestCaseForm from './TestCaseForm'
 
 function App() {
   const theme = createMuiTheme({
     palette: {
-      type: 'dark'
+      type: 'dark',
+      primary: {
+        light: purple[100],
+        main: purple[200],
+        dark: purple[500]
+      }
     }
   })
   return (
