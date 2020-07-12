916fc1563ba3cb8b1b2bd2a8d77f2e032d4a3324
jnhallquist <jnhallquist@gmail.com>
2020-07-11 21:46:18 -0700

    Create Constants file and update statuses references

---
+++ client/src/Constants.js
@@ -0,0 +1,18 @@
+export const STATUSES = [
+  {
+    value: "Untested",
+    label: "Untested",
+  },
+  {
+    value: "Passed",
+    label: "Passed",
+  },
+  {
+    value: "Failed",
+    label: "Failed",
+  },
+  {
+    value: "Blocked",
+    label: "Blocked",
+  },
+];
