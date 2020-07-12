1dbfb4f880f81669834801a3506af2a83b13c0d2
jnhallquist <jnhallquist@gmail.com>
2020-07-11 21:34:52 -0700

    Implement modal dialog that opens from TestCases table

--- client/src/TestCases.js
+++ client/src/TestCases.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Dialog from '@material-ui/core/Dialog';
 import MUIDataTable from "mui-datatables";
 
 import axios from "axios";
 
+import UpdateTestCase from "./UpdateTestCase";
+
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
+
+// const [open, setOpen] = React.useState(false);
 
 const columns = [
   {
@@ -33,7 +38,7 @@ const columns = [
 const options = {
   download: false,
   print: false,
-  viewColumns: false,
+  viewColumns: false
 };
 
 class TestCases extends React.Component {
@@ -42,6 +47,7 @@ class TestCases extends React.Component {
 
     this.state = {
       test_cases: [],
+      dialog_open: false
     };
   }
 
@@ -52,14 +58,35 @@ class TestCases extends React.Component {
     });
   }
 
+  handleClickOpen = () => {
+    this.setState({dialog_open: true})
+  };
+
+  handleClose = () => {
+    this.setState({dialog_open: false})
+  };
+
+  options = {
+    download: false,
+    print: false,
+    viewColumns: false,
+    onRowClick: this.handleClickOpen
+  };
+
   render() {
     return (
+      <div>
       <MUIDataTable
         title={"All Test Cases"}
         data={this.state.test_cases}
         columns={columns}
-        options={options}
+        options={this.options}
       />
+
+      <Dialog open={this.state.dialog_open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
+        <UpdateTestCase onClose={this.handleClose} />
+      </Dialog>
+      </div>
     );
   }
 }
