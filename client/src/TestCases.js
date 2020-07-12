c5d7e4ff75a6cb38f8e1ec708eab981ac607c4e9
jnhallquist <jnhallquist@gmail.com>
2020-07-11 19:03:49 -0700

    Remove console logs

--- client/src/TestCases.js
+++ client/src/TestCases.js
@@ -11,82 +11,48 @@ const columns = [
     name: "title",
     label: "Title",
     options: {
-      filter: false
-    }
+      filter: false,
+    },
   },
   {
     name: "description",
     label: "Description",
     options: {
-      filter: false
-    }
+      filter: false,
+    },
   },
   {
     name: "status",
     label: "Status",
     options: {
-      filter: true
-    }
-  }
-]
-
-const data = [
-  {
-    id: 0,
-    title: "Verify user can login",
-    description: "dankdandna",
-    preconditions: "",
-    steps: "",
-    expected_results: "",
-    postconditions: "",
-    status: "Untested",
-  },
-  {
-    id: 1,
-    title: "Verify user can reset password",
-    description: "dajiejeiifw",
-    preconditions: "",
-    steps: "",
-    expected_results: "",
-    postconditions: "",
-    status: "Blocked",
-  },
-  {
-    id: 2,
-    title: "Verify user can create new account",
-    description: "dajiojiaidjai",
-    preconditions: "",
-    steps: "",
-    expected_results: "",
-    postconditions: "",
-    status: "Pass",
+      filter: true,
+    },
   },
 ];
 
 const options = {
   download: false,
   print: false,
-  viewColumns: false
-}
+  viewColumns: false,
+};
 
 class TestCases extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      test_cases: []
-    }
+      test_cases: [],
+    };
   }
 
   componentDidMount() {
     axios.get("http://localhost:3001/test_cases").then((response) => {
       const test_cases = response.data;
-      this.setState({ test_cases })
+      this.setState({ test_cases });
     });
   }
 
   render() {
-    console.log(123, this.state)
     return (
       <MUIDataTable
         title={"All Test Cases"}
